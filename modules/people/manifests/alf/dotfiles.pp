6b0fac81999111480f3d194259ca80480913e0b0
Alf Lervag <alf.lervag@bouvet.no>
2014-02-03 13:33:43 +0100

    Manually added dotfile links
    This needs to be automated since the current way is clunky and
    repetative.  I'd prefer something like:
    
    dotfile {
      [
        '.zshrc',
        '.zshenv',
        '...'
      ]:
      repository => $dotfiles_dir
    }
    
    Or even better:
    
    dotfiles { "$dotfiles_dir":
      exclude => [ '.git', '.gitignore', 'README.md' ],
      target => "$home"
    }

--- modules/people/manifests/alf/dotfiles.pp
+++ modules/people/manifests/alf/dotfiles.pp
@@ -1,8 +1,15 @@
 class people::alf::dotfiles {
   $home = "/Users/${::boxen_user}"
+
+  $dotfiles_dir = "${boxen::config::srcdir}/dotfiles"
   $emacs_dir = "${boxen::config::srcdir}/emacs.d"
+
   repository { "${emacs_dir}":
       source   => 'alf/emacs.d',
+  }
+
+  repository { "${dotfiles_dir}":
+    source   => 'alf/dotfiles',
   }
 
   file { "${home}/.emacs.d":
@@ -11,4 +18,45 @@ class people::alf::dotfiles {
     require => Repository[$emacs_dir]
   }
 
+  file { "${home}/.zshenv":
+    ensure => link,
+    target => "${dotfiles_dir}/.zshenv",
+    require => Repository[$dotfiles_dir]
+  }
+
+  file { "${home}/.zshrc":
+    ensure => link,
+    target => "${dotfiles_dir}/.zshrc",
+    require => Repository[$dotfiles_dir]
+  }
+
+  file { "${home}/.hgrc":
+    ensure => link,
+    target => "${dotfiles_dir}/.hgrc",
+    require => Repository[$dotfiles_dir]
+  }
+
+  file { "${home}/.pylintrc":
+    ensure => link,
+    target => "${dotfiles_dir}/.pylintrc",
+    require => Repository[$dotfiles_dir]
+  }
+
+  file { "${home}/.screenrc":
+    ensure => link,
+    target => "${dotfiles_dir}/.screenrc",
+    require => Repository[$dotfiles_dir]
+  }
+
+  file { "${home}/.vimrc":
+    ensure => link,
+    target => "${dotfiles_dir}/.vimrc",
+    require => Repository[$dotfiles_dir]
+  }
+
+  file { "${home}/.gitconfig":
+    ensure => link,
+    target => "${dotfiles_dir}/.gitconfig",
+    require => Repository[$dotfiles_dir]
+  }
 }
